91d1588fd3af66c905864922d06ce6e1ce456071
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-06-01 09:58:03 +0200

    buttons component and statrt structure

---
+++ src/components/buttons.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="button" class="button">
+		<button class="button--style">{{ name }}</button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		name: String,
+	},
+}
+</script>
+<style lang="scss">
+.button {
+	display: inline-block;
+	&--style {
+		background-color: #b56a85;
+		color: #000000;
+		height: 26px;
+		width: auto;
+		border-radius: 8px;
+		border: transparent;
+	}
+}
+</style>
---
+++ src/views/MusicApp.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="musicApp">
+		<header class="musicApp__header">
+			<h1 class="musicApp__header--title">{{ title }}</h1>
+		</header>
+		<main class="musicApp__main">
+			<h1 class="musicApp__main--nameMusicPlay">{{ nameMusicPlay }}</h1>
+			<buttons name="Play" />
+			<buttons name="Pause" />
+		</main>
+	</div>
+</template>
+
+<script>
+import buttons from "@/components/buttons.vue"
+
+export default {
+	components: {
+		buttons,
+	},
+	data() {
+		return {
+			title: "This is",
+			nameMusicPlay: "Ariana Grande - 7 Rings",
+		}
+	},
+}
+</script>
+
+<style lang="scss">
+.musicApp {
+	&__header {
+		width: 100%;
+		height: 48px;
+		background: linear-gradient(0.25turn, #b56a85, #373838, #e5e5e5);
+		&--title {
+			color: #ffffff;
+			font-size: 24px;
+			padding-top: 2%;
+		}
+	}
+	&__main {
+		&--nameMusicPlay {
+			font-size: 18px;
+		}
+	}
+}
+</style>
